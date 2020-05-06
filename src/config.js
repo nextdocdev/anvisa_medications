420ef1d21c1c0ac800fa7c6f783704a91fd01e3e
Carlos Barata <carlos.barata@outlook.com>
2020-05-06 19:36:18 -0300

    change to key and sensitive data

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-global.SALT_KEY = "0BEEAEAA-6F91-4A9D-BF4F-87248D08271B";
+global.SALT_KEY = "guid for password";
 
 module.exports = {
-	connectionStrings: 'mongodb+srv://nextdoc:n3xtd0c@cluster0-e9ias.azure.mongodb.net/test?retryWrites=true&w=majority'
+	connectionStrings: 'mongodb+srv://[user]:[password]@cluster0-e9ias.azure.mongodb.net/test?retryWrites=true&w=majority'
 }
 
